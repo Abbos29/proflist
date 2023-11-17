d2f53062dcbcf1a64e2f977f793a2c892921646a
Shaxzod <sanedidi98@gmail.com>
2023-11-17 14:22:33 +0500

    fix: modal

--- script.js
+++ script.js
@@ -2,24 +2,31 @@ const modalBtn1 = document.getElementById('modal1-btn');
 const modal1 = document.querySelector('#modal1');
 const modalBtn2 = document.getElementById('modal2-btn');
 const modal2 = document.querySelector('#modal2');
-const body = document.querySelector('#body');
+const body = document.querySelector('body');
 
-modalBtn1.addEventListener('click', function() {
+modalBtn1.addEventListener('click', function () {
   modal1.classList.toggle('active');
-  body.classList.remove('active');
-});
-modalBtn2.addEventListener('click', function() {
-    modal2.classList.toggle('active');
-    body.classList.remove('active');
+  body.classList.toggle('active');
+
+  body.addEventListener('click', function (event) {
+    if (!modal1.contains(event.target) && !modalBtn1.contains(event.target)) {
+      modal1.classList.remove('active')
+      body.classList.remove('active')
+    }
   });
-  
-
-document.addEventListener('click', function(event) {
-  if (!modal1.contains(event.target) && !modalBtn1.contains(event.target)) {
-    modal1.classList.remove('active');
-  }
-
-  if (!modal2.contains(event.target) && !modalBtn2.contains(event.target)) {
-    modal2.classList.remove('active');
-  }
 });
+
+
+modalBtn2.addEventListener('click', function () {
+  modal2.classList.toggle('active');
+  body.classList.toggle('active');
+  
+  body.addEventListener('click', function (event) {
+    if (!modal2.contains(event.target) && !modalBtn2.contains(event.target)) {
+      modal2.classList.remove('active')
+      body.classList.remove('active')
+    }
+  });
+});
+
+
